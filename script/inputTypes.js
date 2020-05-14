73ba53c71b9a7fb9d16a7ee4f0af40bcf15fc3af
Richard <richard.698.lin@gmail.com>
2020-05-13 22:14:23 -0700

    Added a file that contains input types

---
+++ script/inputTypes.js
@@ -0,0 +1,11 @@
+let coinDef = [
+  { name : 'nickel' , val : 0.05 },
+  { name : 'dime'   , val : 0.10 },
+  { name : 'quarter', val : 0.25 },
+];
+
+let items = [
+  { name : 'item1', cost : 0.55 },
+  { name : 'item2', cost : 0.70 },
+  { name : 'item3', cost : 0.75 },
+];
