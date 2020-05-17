9f171f775523ee69630e6a5d2446ea4bf12cca5b
Richard <richard.698.lin@gmail.com>
2020-05-17 10:37:22 -0700

    Restructed code to be more object orientated

--- web/script/index.js
+++ web/script/index.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
           this.$emit("input", +str);
           this.$refs.input.value = +str;
         } else {
-          openDialog('Error', 'You can only enter in digits.'); 
+          openDialog('Error', 'You can only enter in digits from 0 to 9.'); 
           this.$refs.input.value = this.value;
         }
       },
@@ -48,9 +48,9 @@ document.addEventListener('DOMContentLoaded', function() {
     props : ['coin','loc'],
     methods : {
       putCoinIn : function(loc) {
-        if(vApp.coins[loc].quantity == 0) return;
-        vApp.coins[loc].quantity--;
-        vApp.addCoinToVendor(vApp.coins[loc].name, vApp.coins[loc].val);
+        if(vApp.user.coins[loc].quantity == 0) return;
+        vApp.user.coins[loc].quantity--;
+        vApp.addCoinToVendor(vApp.user.coins[loc].name, vApp.user.coins[loc].val);
       }
     },
     template : `
@@ -82,61 +82,65 @@ document.addEventListener('DOMContentLoaded', function() {
   vApp = new Vue({
     el : '#app',
     data : {
-      coins : [],                     // Coins gather by user
-      vending : [],                   // Items in vending machine
-      coinsInVendor : {},             // Coins in the vending machine
-      itemInBag : [],                 // Items in user's bag
-      sum : 0,                        // Sum of money in vending machine
-      picked : '',                    // Item selected by user
+      user :{
+        coins : [],                     // Coins gather by user
+        itemInBag : [],                 // Items in user's bag
+      },
+      vending : {
+        items : [],                     // Items in vending machine
+        coinsIn : {},                   // Coins in the vending machine
+        sum : 0,                        // Sum of coins in vending machine
+        picked : '',                    // Item selected by user
+      }
     },
     methods : {
       addCoin : function(name, quantity, val) {
-        let coin = this.getElement(this.coins, name);
-        if(coin == null) this.coins.push({ name : name, quantity : 5, val : val });
+        let coin = this.getElement(this.user.coins, name);
+        if(coin == null) this.user.coins.push({ name : name, quantity : 5, val : val });
         else coin.quantity += quantity;
       },
       addItemToVendor : function(name, quantity, cost) {
-        let item = this.getElement(this.vending, name);
-        if(item == null) this.vending.push({ name : name, quantity : 10, cost : cost });
+        let item = this.getElement(this.vending.items, name);
+        if(item == null) this.vending.items.push({ name : name, quantity : 10, cost : cost });
         else item.quantity += quantity;
       },
       addCoinToVendor : function(name, val) {
-        this.coinsInVendor[name] = ++this.coinsInVendor[name] || 1;
-        this.sum += val;
+        this.vending.coinsIn[name] = ++this.vending.coinsIn[name] || 1;
+        this.vending.sum += val;
       },
       getItem : function(evt) {
         let name = evt.value;
-        let item = this.getElement(this.vending, name);
+        let item = this.getElement(this.vending.items, name);
         if(item == null) return;
-        if(item.quantity > 0 && this.sum >= item.cost) {
+        if(item.quantity > 0 && this.vending.sum >= item.cost) {
           // Update item status
           item.quantity--;
-          this.sum -= item.cost;
+          this.vending.sum -= item.cost;
           this.findChange();
-          this.itemInBag.push(item.name);
+          this.user.itemInBag.push(item.name);
 
           // Wait 500 before deselecting radio
-          setTimeout(() => { this.picked = '' }, 500);
+          setTimeout(() => { this.vending.picked = '' }, 500);
         } else {
-          this.picked = '';
-          if(item.quantity == 0) openDialog('Error', `-${item.name}- is out. Funds are refunded.`);
-          else openDialog('Error', `You have insufficient fund ($${getDollars(this.sum)}) in the machine to buy -${name}-. Funds are refunded.`);
+          this.vending.picked = '';
+          if(item.quantity == 0) openDialog('Error', `-${item.name}- is out. Coins are returned.`);
+          else openDialog('Error', `You have insufficient fund ($${getDollars(this.vending.sum)}) in the machine to buy -${name}-. Coins are returned.`);
         }
   
         this.returnCoins();
       },
       returnCoins : function() {
-        for(let coin of this.coins) {
-          if(this.coinsInVendor[coin.name]) coin.quantity += this.coinsInVendor[coin.name]; 
+        for(let coin of this.user.coins) {
+          if(this.vending.coinsIn[coin.name]) coin.quantity += this.vending.coinsIn[coin.name]; 
         }
-        this.coinsInVendor = {};
-        this.sum = 0;
+        this.vending.coinsIn = {};
+        this.vending.sum = 0;
       },
       findChange : function() {
         // Assumed change can only be returned in nickel
-        let numOfNickel = Math.round(this.sum/0.05);
-        this.coinsInVendor = {};
-        this.coinsInVendor['nickel'] = numOfNickel;
+        let numOfNickel = Math.round(this.vending.sum/0.05);
+        this.vending.coinsIn = {};
+        this.vending.coinsIn['nickel'] = numOfNickel;
       },
       getDollars : function(val) {
         return getDollars(val);
